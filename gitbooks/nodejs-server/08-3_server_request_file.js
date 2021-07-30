e060726e469fab5ab726ffd7d597f3356d3ca515
Esantomi <pilinnasa@gmail.com>
2021-07-30 12:18:05 +0900

    a case in which a client asks for a html file. the js file deals with the request

--- gitbooks/nodejs-server/08-3_server_request_file.js
+++ gitbooks/nodejs-server/08-3_server_request_file.js
@@ -17,7 +17,7 @@ var server = http.createServer(function(request,response){
   // 1. 요청된 자원이 /hello 이면
   if(resource == '/hello'){
     // 2. hello.html 파일을 읽은 후
-    fs.readFile('hello.html', 'utf-8', function(error, data) {
+    fs.readFile('html/hello.html', 'utf-8', function(error, data) {
       // 2.1 읽으면서 오류가 발생하면 오류의 내용을
       if(error){
         response.writeHead(500, {'Content-Type':'text/html'});
