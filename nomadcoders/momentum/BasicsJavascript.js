91147c53c89368d2ec37cb8501b0c727cda7ca2c
Esantomi <pilinnasa@gmail.com>
2021-08-05 20:01:12 +0900

    app.js file added

--- nomadcoders/momentum/BasicsJavascript.js
+++
@@ -1,2 +0,0 @@
-// 2.0 Your First JS Project
-alert("hi")  // index.html에서 <script src="app.js"></script>로 이 파일(app.js)을 끌어 쓴다.
---
+++ nomadcoders/momentum/app.js
@@ -0,0 +1,8 @@
+// 3.0 The Document Object
+
+/* JS는 HTML과 상호 작용하기 위해 사용한다.
+즉 HTML의 element를 JS를 통해 읽고 변경할 수 있다는 것이다. */
+
+// document
+
+// alert("hi")  // index.html에서 <script src="app.js"></script>로 현재 파일(app.js)을 끌어 쓴다.
