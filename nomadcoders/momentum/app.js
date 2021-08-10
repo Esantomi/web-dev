edbe8b87f6e7b2ced394e05e2af987ac98cd595e
Esantomi <pilinnasa@gmail.com>
2021-08-10 13:43:31 +0900

    building login-form

--- nomadcoders/momentum/app.js
+++ nomadcoders/momentum/app.js
@@ -3,4 +3,11 @@
 
 
 // 4 [2021 UPDATE] LOGIN
+
 // 4.0 Input Values
+const loginForm = document.querySelector("#login-form");     // id를 가져올 때는 #을 붙여서 id임을 명시해 준다.
+// const loginForm = document.getElementById("login-form");  // 원래 id를 가져오는 method이다.
+
+// input과 button 가져오기
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
