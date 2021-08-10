399cda525c148a7f3fa7dedf0b94401fdbb2110a
Esantomi <pilinnasa@gmail.com>
2021-08-10 14:34:49 +0900

    login button handler and listner setting

--- nomadcoders/momentum/app.js
+++ nomadcoders/momentum/app.js
@@ -8,6 +8,20 @@
 const loginForm = document.querySelector("#login-form");     // id를 가져올 때는 #을 붙여서 id임을 명시해 준다.
 // const loginForm = document.getElementById("login-form");  // 원래 id를 가져오는 method이다.
 
-// input과 button 가져오기
+// input과 button 가져오기: 방법1 (loginForm에서 input 가져오기)
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+
+// input과 button 가져오기: 방법2 (document에서 login-form의 input 가져오기. 2줄로 가능)
+const loginInput2 = document.querySelector("#login-form input");
+const loginButton2 = document.querySelector("#login-form button");
+
+// 로그인 버튼 handler 설정 (loginInput의 value 출력)
+function handleLoginBtnClick() {
+    // console.dir(loginInput);             // loginInput object의 속성(property)을 계층 구조로 출력.
+    console.log("안녕", loginInput.value);  // 기본적으로 value: "". 입력하고 click하면 입력 값을 value로 출력.
+    // console.log("눌렀다!");
+}
+
+// 로그인 버튼 listener 설정
+loginButton.addEventListener("click", handleLoginBtnClick);
