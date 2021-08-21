debe2e066d00beba72daafd18a541bb2672f424f
Esantomi <pilinnasa@gmail.com>
2021-08-21 14:04:50 +0900

    the folder being moved to ../

---
+++ ygl-react/src/Test.js
@@ -0,0 +1,28 @@
+// App.js 전체 복붙
+// index.js의 ReactDOM.render()에 <Test /> 추가
+
+import logo from './logo.svg';
+import './App.css';
+
+function Test() {  // App -> Test로 변경
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Test;  // App -> Test로 변경
--- ygl-react/src/index.js
+++ ygl-react/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Test from './Test';  // 테스트용
 import reportWebVitals from './reportWebVitals';
 
+// 렌더링
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Test />
   </React.StrictMode>,
   document.getElementById('root')
-);
+);  // <Test /> 추가
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
