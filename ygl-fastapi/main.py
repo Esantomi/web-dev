7c3ca143f084734e416bbc32c3678c2481d7c2b3
Esantomi <pilinnasa@gmail.com>
2021-08-21 17:41:24 +0900

    localhost:8000/kokam being added

--- ygl-fastapi/main.py
+++ ygl-fastapi/main.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# localhost:8000
 @app.get("/")
 async def root():
     return { "message" : "Hello World" }
+
+# localhost:8000/kokam
+@app.get("/kokam")
+async def root():
+    return ({ "name" : "Kokam" })
 
 # uvicorn main:app --reload --host=0.0.0.0 --port=8000
