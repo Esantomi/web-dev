fcd04f3d2f386f6242e7c7f875e2c85d398955c1
Esantomi <pilinnasa@gmail.com>
2021-08-21 15:37:51 +0900

    menu building

---
+++ ygl-react/src/Example.js
@@ -0,0 +1,25 @@
+// https://www.npmjs.com/package/@szhsin/react-menu
+
+import React from 'react';
+import {
+    Menu,
+    MenuItem,
+    MenuButton,
+    SubMenu
+} from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
+
+// export 설정 & 함수 정의
+export default function Example() {
+    return (
+        <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
+            <MenuItem>New File</MenuItem>
+            <SubMenu label="Open">
+                <MenuItem>index.html</MenuItem>
+                <MenuItem>example.js</MenuItem>
+                <MenuItem>about.css</MenuItem>
+            </SubMenu>
+            <MenuItem>Save</MenuItem>
+        </Menu>
+    );
+}
