8437054aa598967ea31345dcb49ee68f5b5cdfb7
Esantomi <pilinnasa@gmail.com>
2021-08-21 17:49:59 +0900

    fastapi main.py being modified

--- ygl-fastapi/main.py
+++ ygl-fastapi/main.py
@@ -10,6 +10,18 @@ async def root():
 # localhost:8000/kokam
 @app.get("/kokam")
 async def root():
-    return ({ "name" : "Kokam" })
+    return [
+        {
+            "id": 1,
+            "name": "꼬깜",
+            "image": "https://avatars.githubusercontent.com/u/61646760?v=4"
+        },
+        {
+            "id": 2,
+            "name": "깜꼬",
+            "image": "https://avatars.githubusercontent.com/u/61646760?v=4"
+        }
+    ]
+    # ({ "name" : "Kokam" })
 
 # uvicorn main:app --reload --host=0.0.0.0 --port=8000
