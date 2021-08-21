28c511e63f3d633fe3d159fc059fdea4ca03341f
Esantomi <pilinnasa@gmail.com>
2021-08-21 15:54:02 +0900

    Pet() function being added

--- ygl-react/src/Components/Image/Image.js
+++ ygl-react/src/Components/Image/Image.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-function Friend(props) {
-    return <h1>{props.name + "입니다."}</h1>
+function Pet(props) {
+    return (
+        <div>
+            <h1>{props.name + "입니다."}</h1>
+            <h2>{props.age + "살입니다."}</h2>
+            <h3>꼬깜 사진(우)<img src={props.image} width="300" height="300"></img>꼬깜 사진(좌)</h3>
+        </div>
+    )
 }
 
 function Image() {
     return (
         <div>
-            <Friend name="꼬깜"></Friend>
+            <Pet name="꼬깜" age="2" image="https://avatars.githubusercontent.com/u/61646760?v=4"></Pet>
             <img src="https://avatars.githubusercontent.com/u/61646760?v=4"></img>
         </div>
     )
