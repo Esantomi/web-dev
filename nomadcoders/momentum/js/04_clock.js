1ab08492d10637b5985cbfe3d367617a478f73c1
Esantomi <pilinnasa@gmail.com>
2021-08-22 17:22:47 +0900

    5.0 Intervals: setInterval()

--- nomadcoders/momentum/js/04_clock.js
+++ nomadcoders/momentum/js/04_clock.js
@@ -2,5 +2,19 @@
 // https://nomadcoders.co/javascript-for-beginners/lobby
 
 
+// 5.0 Intervals
+// Interval: 일정 간격으로 발생
 
-// 
+// HTML에서 clock 가져오기
+const clock = document.querySelector("h2#clock");  // 그냥 #clock도 가능
+
+// 예제용 함수
+function sayHello() {
+    console.log("Hello");
+}
+
+// interval 설정 (반복할 함수명, 시간 간격(ms))
+setInterval(sayHello, 5000);  // 5초마다 콘솔에 Hello 출력
+
+
+// 5.1 Timemouts and Dates
