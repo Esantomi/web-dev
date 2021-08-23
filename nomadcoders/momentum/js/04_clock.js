3f627fa948acd68c0f7b68360431b388c1a39e1e
Esantomi <pilinnasa@gmail.com>
2021-08-23 20:17:13 +0900

    Date object and its methods (getHours(), getMinutes() ...)

--- nomadcoders/momentum/js/04_clock.js
+++ nomadcoders/momentum/js/04_clock.js
@@ -23,3 +23,25 @@ setInterval(sayHello, 5000);  // 5초마다 콘솔에 Hello 출력
 // Timeout 설정 (반복할 함수명, 시간 간격(ms))
 setTimeout(sayHello, 5000);  // 5초 후에 콘솔에 Hello 1번 출력
 
+// Date object
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Date
+console.log(Date);        // Date object 출력 : ƒ Date() { [native code] }
+console.log(new Date());  // 한국 표준시 기준 현재 연월일시 출력
+
+const date = new Date();          // Date 객체 생성
+// console.log(date.getDate());      // 23
+// console.log(date.getDay());       // 1 (월요일)
+// console.log(date.getFullYear());  // 2021
+// console.log(date.getHours());     // 19 (시)
+// console.log(date.getMinutes());   // 53 (분)
+// console.log(date.getSeconds());   // 29 (초)
+
+// 새 Date object를 만들고, 새 object의 시, 분, 초를 출력하는 함수
+function getClock() {
+    const date = new Date();
+    // console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+getClock();                   // 페이지 열자마자 함수 호출
+setInterval(getClock, 1000);  // interval 설정 : 1초 간격
