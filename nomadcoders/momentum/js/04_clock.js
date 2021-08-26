6a4f7b3326d3b30dff90c78ffe50a1c8df1206b1
Esantomi <pilinnasa@gmail.com>
2021-08-26 12:58:25 +0900

    padStart() & padEnd() functions

--- nomadcoders/momentum/js/04_clock.js
+++ nomadcoders/momentum/js/04_clock.js
@@ -37,11 +37,38 @@ const date = new Date();          // Date 객체 생성
 // console.log(date.getSeconds());   // 29 (초)
 
 // 새 Date object를 만들고, 새 object의 시, 분, 초를 출력하는 함수
-function getClock() {
+function getClockPrototype() {
     const date = new Date();
     // console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
     clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-getClock();                   // 페이지 열자마자 함수 호출
-setInterval(getClock, 1000);  // interval 설정 : 1초 간격
+// getClockPrototype();                   // 페이지 열자마자 함수 호출
+// setInterval(getClockPrototype, 1000);  // interval 설정 : 1초 간격
+
+
+// 5.2 PadStart
+// padStart() : 특정 string(= padding)을 붙여 전체 string의 길이를 맞춰 주는 함수
+console.log("1".padStart(2, "0"));   // "1"을 2자리로, 앞에 "0"을 넣어서 맞춤
+console.log("12".padStart(2, "0"));  // 이미 2자리이므로 12 출력
+
+// padEnd() : padding을 맨 뒤에 붙여 주는 함수
+console.log("1".padEnd(2, "0"));       // 10 출력
+console.log("hello".padEnd(20, "X"));  // helloXXXXXXXXXXXXXXX 출력
+
+// padding을 붙여 개선한 getClock 함수
+function getClock() {
+    const date = new Date();
+
+    // Date의 properties는 number이므로 padStart를 그대로 사용할 수 없다.
+    const hours = String(date.getHours()).padStart(2, "0");  // 따라서 string으로 만든 뒤 padding을 붙인다.
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
+
+
+// 5.3 Recap
