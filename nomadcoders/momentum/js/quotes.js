226786d56ea8af200a7ac69581d828f574b1bd6c
Esantomi <pilinnasa@gmail.com>
2021-08-30 18:57:33 +0900

    quotes array being added

--- nomadcoders/momentum/js/quotes.js
+++ nomadcoders/momentum/js/quotes.js
@@ -4,5 +4,44 @@
 
 // quotes
 const quotes = [
-    {}
+    {
+        quote: "A fox knows many things, but a hedgehog knows one big thing.",
+        author: "Archilochus"
+    },
+    {
+        quote: "There is nothing permanent except change.",
+        author: "Heraclitus"
+    },
+    {
+        quote: "Death does not concern us, because as long as we exist, death is not here. And when it does come, we no longer exist.",
+        author: "Epicurus"
+    },
+    {
+        quote: "Excess of grief for the dead is madness; for it is an injury to the living, and the dead know it not.",
+        author: "Xenophon"
+    },
+    {
+        quote: "Man: a being in search of meaning.",
+        author: "Plato"
+    },
+    {
+        quote: "I don't need a friend who changes when I change and who nods when I nod; my shadow does that much better.",
+        author: "Plutarch"
+    },
+    {
+        quote: "History is Philosophy teaching by examples.",
+        author: "Thucydides"
+    },
+    {
+        quote: "blank 1",
+        author: "someone"
+    },
+    {
+        quote: "blank 2",
+        author: "someone"
+    },
+    {
+        quote: "blank 3",
+        author: "someone"
+    }
 ]
