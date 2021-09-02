172fa6b6125b52095859e9d54cefe9bf3b83ed7a
Esantomi <pilinnasa@gmail.com>
2021-09-02 16:00:46 +0900

    quotes.js file shows random quotes and their authors

--- nomadcoders/momentum/js/quotes.js
+++ nomadcoders/momentum/js/quotes.js
@@ -50,35 +50,6 @@ const quotes = [
 const quote = document.querySelector("#quote span:first-child");  // HTML id=quote의 첫 번째 span 가져오기
 const author = document.querySelector("#quote span:last-child");  // HTML id=quote의 마지막 span 가져오기
 
-// quote 출력
-console.log(quotes[0]);       // 첫 번째 quote
-console.log(quotes[10 - 1]);  // 마지막 quote
-
-
-// Math 모듈 테스트
-console.log(Math.PI);  // pi 값 출력
-
-// Math.random()
-console.log(Math.random());       // 0~1 사이의 숫자 무작위 반환
-console.log(Math.random() * 10);  // 0~10 사이의 숫자 무작위 반환
-
-// 소수점 뒷자리 제거용 함수
-console.log(Math.round(1.5));   // 2. 반올림
-console.log(Math.ceil(1.01));   // 2. 올림
-console.log(Math.floor(2.9));   // 2. 버림
-
-// 0부터 9 사이의 숫자를 무작위로 반환하는 함수
-console.log(Math.floor(Math.random() * 10));
-
-// quotes array의 0~9 element를 무작위로 출력
-console.log(quotes[Math.floor(Math.random() * 10)]);  // 10을 쓴 건 소스 코드에 데이터를 직접 입력하는 '하드 코딩'이라 유지 보수가 번거롭다.
-
-// [].length : array의 길이 반환
-console.log([1,2,3,4,5,6,7].length);  // 7
-
-// length를 활용해 10을 대체한 문장
-console.log(quotes[Math.floor(Math.random() * quotes.length)]);
-
 // 0~quotes 길이 사이의 quotes array element를 무작위로 반환하는 문장을 변수에 할당
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
