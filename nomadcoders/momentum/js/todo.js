e6ca1b5caaee82b5dc3b48b3093474777357d535
Esantomi <pilinnasa@gmail.com>
2021-09-03 19:36:56 +0900

    making a handletoDoSubmit method

--- nomadcoders/momentum/js/todo.js
+++ nomadcoders/momentum/js/todo.js
@@ -2,3 +2,18 @@
 // https://nomadcoders.co/javascript-for-beginners/lobby
 
 
+// const toDoForm = document.querySelector("#todo-form");
+const toDoForm = document.getElementById("todo-form");
+const toDoList = document.getElementById("todo-list");
+// const toDoInput = document.querySelector("#todo-form input");  // 아래와 동일
+const toDoInput = toDoForm.querySelector("input");
+
+// js가 발생한 event를 함수의 첫 번째 인자로 넘겨 줌
+function handleToDoSubmit(e) {
+    e.preventDefault();
+    console.log(toDoInput.value);  // 입력 값 출력
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// 5:15부터 계속
