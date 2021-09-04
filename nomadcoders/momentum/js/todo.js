a6a37a9af168782f569343f5c0087e466d95d82c
Esantomi <pilinnasa@gmail.com>
2021-09-04 15:58:04 +0900

    #7.1 Adding toDos

--- nomadcoders/momentum/js/todo.js
+++ nomadcoders/momentum/js/todo.js
@@ -14,10 +14,22 @@ function handleToDoSubmit(e) {
     e.preventDefault();
     const newToDo = toDoInput.value;        // 입력 값 변수 할당
     toDoInput.value = "";                   // 이후 input 칸 비우기
-    console.log(newToDo, toDoInput.value);  // 입력 값 출력 (후자는 안 보임)
+    // console.log(newToDo, toDoInput.value);  // 입력 값 출력 (후자는 안 보임)
+    paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
 
 // 7.1 Adding ToDos
+function paintToDo(newToDo) {
+    // console.log("이걸 그릴 거야:", newToDo);  // 테스트용
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);  // li 안에 span 넣기
+    span.innerText = newToDo;
+    // console.log(li);  // <li><span>입력 값</span></li>
+    toDoList.appendChild(li);
+}
+
+// 삭제가 불가하다는 것과 새로고침하면 ToDos가 사라진다는 문제점이 있음
