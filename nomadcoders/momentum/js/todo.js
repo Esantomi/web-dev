b7392b60be4077a03d80e508f846b5595e98ee40
Esantomi <pilinnasa@gmail.com>
2021-09-04 14:32:26 +0900

    #7.0 Setup

--- nomadcoders/momentum/js/todo.js
+++ nomadcoders/momentum/js/todo.js
@@ -2,6 +2,7 @@
 // https://nomadcoders.co/javascript-for-beginners/lobby
 
 
+//7.0 Setup
 // const toDoForm = document.querySelector("#todo-form");
 const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
@@ -11,9 +12,12 @@ const toDoInput = toDoForm.querySelector("input");
 // js가 발생한 event를 함수의 첫 번째 인자로 넘겨 줌
 function handleToDoSubmit(e) {
     e.preventDefault();
-    console.log(toDoInput.value);  // 입력 값 출력
+    const newToDo = toDoInput.value;        // 입력 값 변수 할당
+    toDoInput.value = "";                   // 이후 input 칸 비우기
+    console.log(newToDo, toDoInput.value);  // 입력 값 출력 (후자는 안 보임)
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-// 5:15부터 계속
+
+// 7.1 Adding ToDos
